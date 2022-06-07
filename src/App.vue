ce4b66d3bda9fa0b1665484ce6c1ad8aeb13c77d
右究 <aisowe@sina.com>
2022-06-07 17:47:36 +0800

    feat: add mockjs

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Mock from 'mockjs'
+</script>
 
-<template>Binary Permissions.</template>
+<template>{{ Mock.mock('@cname') }}</template>
